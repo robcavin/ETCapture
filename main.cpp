f938e91dc876bf4040c40da945000d3904d43132
Robert Cavin <robert.cavin@oculus.com>
2019-05-31 16:20:52 -0700

    Arguably capturing something

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
 #include <ov580.h>
 #include <pangolin/video/video_input.h>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <linux/usb/video.h>
 #include <linux/uvcvideo.h>
 
+bool record = false;
+int mouse_x, mouse_y;
 
 void mouseCallback(int event, int x, int y, int flags, void *userdata) {
+    if( event == cv::EVENT_LBUTTONDOWN ) record = true;
+    if( event == cv::EVENT_LBUTTONUP ) record = false;
+    mouse_x = x;
+    mouse_y = y;
 }
 
 int main() {
 
-    cvNamedWindow("Test", CV_WINDOW_NORMAL);
-    //cvSetWindowProperty("Test", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
-    cvSetMouseCallback("Test", mouseCallback);
+    cvNamedWindow("ETCapture", CV_WINDOW_NORMAL);
+    cvSetWindowProperty("ETCapture", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
 
-    //cv::waitKey();
+    auto window_size = cv::Size(2560,1600);
+
+    cvSetMouseCallback("ETCapture", mouseCallback);
+
+    auto target = cv::imread("../target.png");
+    cv::resize(target,target,cv::Size(64,64));
 
     surreal::RegisterOv580VideoFactory();
     pangolin::VideoInput video;
@@ -33,18 +43,36 @@ int main() {
 
     bool run = true;
 
+    int image_number = 1;
+
     while (run) {
-        cv::Mat cv_img;
+
+        // Update eye image
+        cv::Mat eye_image;
         if (haveVideo) {
             video.Grab(buffer,images,true,true);
             const auto& img = images[0];
-            cv_img = cv::Mat(img.h,img.w,CV_8U, img.ptr, img.pitch);
+            eye_image = cv::Mat(img.h,img.w * 2,CV_8U, img.ptr, img.pitch);
         } else {
-            cv_img = cv::Mat(480,1280,CV_8U,cv::Scalar(127));
+            eye_image = cv::Mat(480,1280,CV_8U,cv::Scalar(256));
         }
+        cv::imshow("Eye image",eye_image);
 
-        cv::imshow("test",cv_img);
-        cv::waitKey(1);
+        cv::Mat background(window_size.height, window_size.width, CV_8U, cv::Scalar(127));
+        cv::Rect rect(mouse_x - target.size().width/2,mouse_y - target.size().height/2, target.size().width, target.size().height);
+        background(rect & cv::Rect({0,0},window_size)) = 0;
+        cv::imshow("ETCapture",background);
+        cv::waitKey(20);
+
+        if (record) {
+            char filename[64];
+            sprintf(filename, "img%06d.jpg", image_number);
+            cv::imwrite(filename, eye_image);
+            sprintf(filename, "img%06d.json", image_number);
+            auto file = fopen(filename, "w+");
+            fprintf(file, "{\"pt\":[%d,%d]}", mouse_x, mouse_y);
+            image_number++;
+        }
     }
 
 }
